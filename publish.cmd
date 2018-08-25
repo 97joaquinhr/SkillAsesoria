62f8580c1018edb46ea4b9ed853ee191f5f55ca2
97joaquinhr <97joaquinhr@gmail.com>
2018-08-25 12:33:00 -0500

    init commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=skilltec-Web-Deploy.pubxml -p:Password=nS7FW1leujrssZu6aTvraibRwsy8NBHDiBvgKWZrRDBcsxCDL2DLh4ZrxGDd
+

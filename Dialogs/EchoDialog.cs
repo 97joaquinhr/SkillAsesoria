9ec3ad91ef6e0dd9357c6b2fccfc4fa33934f879
97joaquinhr <97joaquinhr@gmail.com>
2018-08-25 13:19:05 -0500

    prueba deploy

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             else
             {
-                await context.PostAsync($"{this.count++}: You said trololol {message.Text}");
+                await context.PostAsync($"{this.count++}: You said vazquez {message.Text}");
                 context.Wait(MessageReceivedAsync);
             }
         }
